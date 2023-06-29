530836e2654dbc566886de5b2f77e4c963b73451
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-06-28 22:13:58 -0300

    Monta estrutura

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,42 @@
 import { Header } from "../../components/Header";
+import background from "../../assets/background.png";
+import ItemList from "../../components/ItemList";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app" />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/84539855?v=4"
+              className="profile"
+              alt="imagem de perfil"
+            />
+            <div>
+              <h3>Diego Abenicio</h3>
+              <span>@diegoabenicio</span>
+              <p>
+                I am seeking a professional opportunity that allows for my
+                intellectual, professional, and technical growth, so that I can
+                contribute to the company for a long
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div>
+            <h4 className="repositorio">Repositorios</h4>
+            <ItemList title="Teste 1" description="descripiton" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
