1ce3d3001e28b97cb3dc7eb758036f7db9587e0e
Diego Alípio Abenicio <diego.abenicio1@gmail.com>
2023-06-29 23:25:15 -0300

    Inicializa fetch

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,31 @@
+import { useState } from "react"; 
 import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
 import ItemList from "../../components/ItemList";
 import "./styles.css";
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentuUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () =>{
+    const userData = await fetch('https://api.github.com/users/${user}');
+    const newUser = await userData.json();
+
+    if(newUser.name){
+      const {avatar_url, name, bio} = newUser;
+      setCurrentUser({ avatar_url, name, bio });
+    
+      const reposData = await fetch("https://api.github.com/users/${user}/repos");
+      const newRepos = await reposData.json();
+
+      if(newRepos.lenght){
+        setRepos(newRepos);
+      }
+    }
+  }
+
   return (
     <div className="App">
       <Header />
@@ -11,30 +33,41 @@ function App() {
         <img src={background} className="background" alt="background app" />
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username" />
-            <button>Buscar</button>
-          </div>
-          <div className="perfil">
-            <img
-              src="https://avatars.githubusercontent.com/u/84539855?v=4"
-              className="profile"
-              alt="imagem de perfil"
+            <input
+              name="usuario"
+              value={user}
+              onChange={(event) => setUser(event.target.value)}
+              placeholder="@username"
             />
+            <button onClick={handleGetData}>Buscar</button>
+          </div>
+          {currentuUser?.name ? (
+            <>
+              <div className="perfil">
+                <img
+                  src="https://avatars.githubusercontent.com/u/84539855?v=4"
+                  className="profile"
+                  alt="imagem de perfil"
+                />
+                <div>
+                  <h3>Diego Abenicio</h3>
+                  <span>@diegoabenicio</span>
+                  <p>
+                    I am seeking a professional opportunity that allows for my
+                    intellectual, professional, and technical growth, so that I
+                    can contribute to the company for a long
+                  </p>
+                </div>
+              </div>
+              <hr />
+            </>
+          ) : null}
+          {repos?.lenght ? (
             <div>
-              <h3>Diego Abenicio</h3>
-              <span>@diegoabenicio</span>
-              <p>
-                I am seeking a professional opportunity that allows for my
-                intellectual, professional, and technical growth, so that I can
-                contribute to the company for a long
-              </p>
+              <h4 className="repositorio">Repositorios</h4>
+              <ItemList title="Teste 1" description="descripiton" />
             </div>
-          </div>
-          <hr />
-          <div>
-            <h4 className="repositorio">Repositorios</h4>
-            <ItemList title="Teste 1" description="descripiton" />
-          </div>
+          ): null}
         </div>
       </div>
     </div>
